96d0bc1509270fd0008e42fae720f44c1e8cf9a4
nakamura <yuki37379@gmail.com>
2021-09-17 21:37:57 +0900

    連絡する必要がなくなり乙なので修正

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"log"
-	"os"
 
-	"github.com/nakamura5545/weather_line_bot/eventhandler"
 	"github.com/nakamura5545/weather_line_bot/infrastracture/line"
 	"github.com/nakamura5545/weather_line_bot/weather"
 )
@@ -16,13 +14,6 @@ func main() {
 	// テキストメッセージを友達登録しているユーザー全員に配信する
 	if _, err := client.BroadcastMessage(message).Do(); err != nil {
 		log.Fatal(err)
-	}
-	// 連絡をしたかどうかのリマインダーを送る(水曜日に出力する)
-	if eventhandler.GetWeek() == 3 {
-		remind := line.TextMessage(os.Getenv("REMIND_MESSAGE"))
-		if _, err := client.BroadcastMessage(remind).Do(); err != nil {
-			log.Fatal(err)
-		}
 	}
 
 }
